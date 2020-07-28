e54ac64aa0d5e729046bfbf1543304468d09c53e
Nattapong Pullkhow <rexenatt@gmail.com>
2020-07-28 10:21:39 +0700

    Fix bug

--- showtime@xenlism.github.io/extension.js
+++ showtime@xenlism.github.io/extension.js
@@ -3,19 +3,29 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
 const Util = imports.misc.util;
+const GLib = imports.gi.GLib;
 const ExtensionPath = imports.misc.extensionUtils.getCurrentExtension().path;
 
 let text, button;
 let showtime_script = ExtensionPath + '/showtime.js';
 
+function killshowtime() {
+  GLib.spawn_command_line_sync("/usr/bin/pkill -f " + showtime_script);
+}
+
+function startshowtime() {
+  Util.spawnCommandLine(" /usr/bin/gjs " + showtime_script);
+}
 function init() {
-     Util.spawnCommandLine(" /usr/bin/gjs " + showtime_script);
+    killshowtime();
+    startshowtime();
 }
 
 function enable() {
-
+  killshowtime();
+  startshowtime();
 }
 
 function disable() {
-
+  killshowtime();
 }
